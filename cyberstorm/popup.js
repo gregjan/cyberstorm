35928fa23d7d306d2ab121a1e8cf418340fc0d52
Gregory Jansen <count0@email.unc.edu>
2013-03-31 11:29:42 -0400

    beginnings

--- cyberstorm/popup.js
+++ cyberstorm/popup.js
@@ -1,128 +1,57 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+function start() {
+	var diameter = 960;
+	var tree = d3.layout.tree().size([ 360, diameter / 2 - 120 ]).separation(
+			function(a, b) {
+				return (a.parent == b.parent ? 1 : 2) / a.depth;
+			});
 
-// Search the bookmarks when entering the search keyword.
-$(function() {
-  $('#search').change(function() {
-     $('#bookmarks').empty();
-     dumpBookmarks($('#search').val());
-  });
-});
-// Traverse the bookmark tree, and print the folder and nodes.
-function dumpBookmarks(query) {
-  var bookmarkTreeNodes = chrome.bookmarks.getTree(
-    function(bookmarkTreeNodes) {
-      $('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes, query));
-    });
-}
-function dumpTreeNodes(bookmarkNodes, query) {
-  var list = $('<ul>');
-  var i;
-  for (i = 0; i < bookmarkNodes.length; i++) {
-    list.append(dumpNode(bookmarkNodes[i], query));
-  }
-  return list;
-}
-function dumpNode(bookmarkNode, query) {
-  if (bookmarkNode.title) {
-    if (query && !bookmarkNode.children) {
-      if (String(bookmarkNode.title).indexOf(query) == -1) {
-        return $('<span></span>');
-      }
-    }
-    var anchor = $('<a>');
-    anchor.attr('href', bookmarkNode.url);
-    anchor.text(bookmarkNode.title);
-    /*
-     * When clicking on a bookmark in the extension, a new tab is fired with
-     * the bookmark url.
-     */
-    anchor.click(function() {
-      chrome.tabs.create({url: bookmarkNode.url});
-    });
-    var span = $('<span>');
-    var options = bookmarkNode.children ?
-      $('<span>[<a href="#" id="addlink">Add</a>]</span>') :
-      $('<span>[<a id="editlink" href="#">Edit</a> <a id="deletelink" ' +
-        'href="#">Delete</a>]</span>');
-    var edit = bookmarkNode.children ? $('<table><tr><td>Name</td><td>' +
-      '<input id="title"></td></tr><tr><td>URL</td><td><input id="url">' +
-      '</td></tr></table>') : $('<input>');
-    // Show add and edit links when hover over.
-        span.hover(function() {
-        span.append(options);
-        $('#deletelink').click(function() {
-          $('#deletedialog').empty().dialog({
-                 autoOpen: false,
-                 title: 'Confirm Deletion',
-                 resizable: false,
-                 height: 140,
-                 modal: true,
-                 overlay: {
-                   backgroundColor: '#000',
-                   opacity: 0.5
-                 },
-                 buttons: {
-                   'Yes, Delete It!': function() {
-                      chrome.bookmarks.remove(String(bookmarkNode.id));
-                      span.parent().remove();
-                      $(this).dialog('destroy');
-                    },
-                    Cancel: function() {
-                      $(this).dialog('destroy');
-                    }
-                 }
-               }).dialog('open');
-         });
-        $('#addlink').click(function() {
-          $('#adddialog').empty().append(edit).dialog({autoOpen: false,
-            closeOnEscape: true, title: 'Add New Bookmark', modal: true,
-            buttons: {
-            'Add' : function() {
-               chrome.bookmarks.create({parentId: bookmarkNode.id,
-                 title: $('#title').val(), url: $('#url').val()});
-               $('#bookmarks').empty();
-               $(this).dialog('destroy');
-               window.dumpBookmarks();
-             },
-            'Cancel': function() {
-               $(this).dialog('destroy');
-            }
-          }}).dialog('open');
-        });
-        $('#editlink').click(function() {
-         edit.val(anchor.text());
-         $('#editdialog').empty().append(edit).dialog({autoOpen: false,
-           closeOnEscape: true, title: 'Edit Title', modal: true,
-           show: 'slide', buttons: {
-              'Save': function() {
-                 chrome.bookmarks.update(String(bookmarkNode.id), {
-                   title: edit.val()
-                 });
-                 anchor.text(edit.val());
-                 options.show();
-                 $(this).dialog('destroy');
-              },
-             'Cancel': function() {
-                 $(this).dialog('destroy');
-             }
-         }}).dialog('open');
-        });
-        options.fadeIn();
-      },
-      // unhover
-      function() {
-        options.remove();
-      }).append(anchor);
-  }
-  var li = $(bookmarkNode.title ? '<li>' : '<div>').append(span);
-  if (bookmarkNode.children && bookmarkNode.children.length > 0) {
-    li.append(dumpTreeNodes(bookmarkNode.children, query));
-  }
-  return li;
+	var diagonal = d3.svg.diagonal.radial().projection(function(d) {
+		return [ d.y, d.x / 180 * Math.PI ];
+	});
+
+	var svg = d3.select("body").append("svg").attr("width", diameter).attr(
+			"height", diameter - 150).append("g").attr("transform",
+			"translate(" + diameter / 2 + "," + diameter / 2 + ")");
+
+	var bookmarkTreeNodes = chrome.bookmarks
+			.getSubTree('0', function(bookmarkTreeNodes) {
+				for(i=0;i<bookmarkTreeNodes.length;i++){
+					    //alert(bookmarkTreeNodes[i].id);
+					    //bookmarkTreeNodes[i].name = 'fooo';
+					    //alert(bookmarkTreeNodes[i].name);
+					    //bookmarkTreeNodes[i].name = bookmarkTreeNodes[i].title;
+				}
+				var nodes = tree.nodes(bookmarkTreeNodes), links = tree
+						.links(nodes);
+
+				var link = svg.selectAll(".link").data(links).enter().append(
+						"path").attr("class", "link").attr("d", diagonal);
+				var node = svg.selectAll(".node").data(nodes).enter().append(
+						"g").attr("class", "node").attr(
+						"transform",
+						function(d) {
+							return "rotate(" + (d.x - 90) + ")translate(" + d.y
+									+ ")";
+						});
+
+				node.append("circle").attr("r", 4.5);
+
+				node.append("text").attr("dy", ".31em").attr("text-anchor",
+						function(d) {
+							return d.x < 180 ? "start" : "end";
+						}).attr(
+						"transform",
+						function(d) {
+							return d.x < 180 ? "translate(8)"
+									: "rotate(180)translate(-8)";
+						}).text(function(d) {
+					return d.name;
+				});
+			});
+
+	d3.select(self.frameElement).style("height", diameter - 150 + "px");
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  dumpBookmarks();
+document.addEventListener('DOMContentLoaded', function() {
+	start();
 });
